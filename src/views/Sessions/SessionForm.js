7eb43dca0cdf58b1fb88614113077175f82b2329
Snehal Kale <snehal.kale@eternussolutions.com>
2018-07-20 17:11:45 +0530

    added basic structure of session module

---
+++ src/views/Sessions/SessionForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+class SessionForm extends Component {
+   
+    componentWillMount(){
+
+    }
+
+
+    render(){
+        return(
+            <div> <h1> hello from sessions </h1> </div>
+        )
+    }
+}
+
+
+
+const mapStateToProps = ()=>{
+    return{
+
+    }
+}
+
+const matchDispatchToProps= () => {
+    return{
+
+    }
+}
+export default connect(mapStateToProps, matchDispatchToProps) (SessionForm);
